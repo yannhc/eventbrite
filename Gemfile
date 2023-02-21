6b791ee3eb072d5c3307a505d6791aec04420a51
yannhc <yann.hc@hotmail.fr>
2023-02-21 10:58:22 +0100

    second heroku

--- Gemfile
+++ Gemfile
@@ -3,6 +3,8 @@ git_source(:github) { |repo| "https://github.com/#{repo}.git" }
 
 ruby "3.0.0"
 
+gem 'devise'
+
 gem 'table_print'
 
 gem 'faker'
--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,4 @@
 class ApplicationMailer < ActionMailer::Base
-  default from: "from@example.com"
+  default from: "yann_hc@hotmail.fr"
   layout "mailer"
 end
